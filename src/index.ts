5977002ca995a55e7364e4854d6fe4fdb4031dd7
luis rivas <luisricardorivasgiwencer@gmail.com>
2024-03-13 00:43:21 -0400

    .env added

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,36 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
-
-import { randomBytes } from 'crypto';
+import { randomBytes } from "crypto";
 
 const app = express();
-const port = 3000;
-
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+const SIZE: number = parseInt(process.env.SECRETSIZE as string) || 64;
 
 app.use(bodyParser.json());
 
-
 const secrets: { [key: string]: string } = {};
 
-
-
 function generateSecretKey(): string {
-  return randomBytes(4).toString('hex');
+  return randomBytes(64).toString("hex");
 }
 
-
-app.post('/api/secret', (req: Request, res: Response) => {
+app.post("/api/secret", (req: Request, res: Response) => {
   const { message } = req.body;
   if (!message) {
-    return res.status(400).json({ error: 'Message is required' });
+    return res.status(400).json({ error: "Message is required" });
   }
   const secretKey = generateSecretKey();
   secrets[secretKey] = message;
   res.status(201).json({ secretKey });
 });
 
-
-app.get('/api/secret/:secretKey', (req: Request, res: Response) => {
+app.get("/api/secret/:secretKey", (req: Request, res: Response) => {
   const { secretKey } = req.params;
   if (!secrets.hasOwnProperty(secretKey)) {
-    return res.status(404).json({ error: 'Secret not found' });
+    return res.status(404).json({ error: "Secret not found" });
   }
 
   const secretMessage = secrets[secretKey];
@@ -42,7 +38,6 @@ app.get('/api/secret/:secretKey', (req: Request, res: Response) => {
   delete secrets[secretKey];
 });
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
