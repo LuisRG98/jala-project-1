e9e13abec2787121a48245843af3a39db2fcfb69
luis rivas <luisricardorivasgiwencer@gmail.com>
2024-03-15 07:19:03 -0400

    add coverage

--- tests/controllers/secretsController.test.ts
+++ tests/controllers/secretsController.test.ts
@@ -1,55 +1,31 @@
-import { generateSecretKey, secrets } from "../../src/utils/index";
-
 import request from "supertest";
 import app from "../../src/app";
+import * as utils from "../../src/utils/index";
 
-describe("Secrets Controller", () => {
-  afterEach(() => {
-    Object.keys(secrets).forEach((key) => {
-      delete secrets[key];
-    });
-  });
+jest.mock("../../src/utils/index", () => ({
+  ...jest.requireActual("../../src/utils/index"),
+  generateSecretKey: jest.fn().mockReturnValue("mocked-secret-key"),
+}));
 
-  it("should create a new secret", async () => {
+describe("API Integration Tests", () => {
+  it("should create a new secret with mocked key", async () => {
     const message = "Hello, world!";
-    const secretKey = generateSecretKey();
-    const response = await request(app)
-      .post("/api/secrets")
-      .send({ message, secretKey })
-      .expect(201);
 
-    expect(response.body).toHaveProperty("secretKey", secretKey);
-    expect(secrets).toHaveProperty(secretKey, message);
+    const response = await request(app).post("/api/secrets").send({ message });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("secretKey", "mocked-secret-key");
   });
 
-  it("should return an error when creating a secret without a message", async () => {
-    const response = await request(app)
-      .post("/api/secrets")
-      .send({})
-      .expect(400);
-
-    expect(response.body).toHaveProperty("error", "Message is required");
-  });
-
-  it("should retrieve a secret", async () => {
+  it("should retrieve a secret with mocked key", async () => {
     const message = "Hello, world!";
-    const secretKey = generateSecretKey();
-    secrets[secretKey] = message;
+    const secretKey = "mocked-secret-key";
 
-    const response = await request(app)
-      .get(`/api/secrets/${secretKey}`)
-      .expect(200);
+    utils.secrets[secretKey] = message;
 
+    const response = await request(app).get(`/api/secrets/${secretKey}`);
+
+    expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("secretMessage", message);
-  });
-
-  it("should return an error when retrieving a non-existent secret", async () => {
-    const nonExistentSecretKey = "non-existent-secret-key";
-
-    const response = await request(app)
-      .get(`/api/secrets/${nonExistentSecretKey}`)
-      .expect(404);
-
-    expect(response.body).toHaveProperty("error", "Secret not found");
   });
 });
